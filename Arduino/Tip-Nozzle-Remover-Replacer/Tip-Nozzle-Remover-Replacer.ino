aecaa7c76e79bbc88dd6408f1302f2afe8490f9f
Alex Arbogast <arbogastaw@gmail.com>
2020-01-21 22:53:14 -0500

    Motor and limit switch functionality for tip removal

--- Arduino/Tip-Nozzle-Remover-Replacer/Tip-Nozzle-Remover-Replacer.ino
+++ Arduino/Tip-Nozzle-Remover-Replacer/Tip-Nozzle-Remover-Replacer.ino
@@ -1,7 +1,30 @@
-char receivedChar;
+/*
+ * TIP+NOZZLE REMOVER+REPLACER 
+ * Alex Arbogast
+ * January 21, 2019
+ */
+#include "Servo.h"
 
-void removeNozzle() { }
-void removeTip() { }
+#define MOTOR_PIN 9
+#define LIMIT_SWITCH 52
+
+char receivedChar;
+Servo motor;
+
+void removeNozzle() 
+{
+    motor.writeMicroseconds(2000);
+
+    while (digitalRead(LIMIT_SWITCH) != 0)
+    {
+        // continue motor
+    }
+
+    motor.writeMicroseconds(1500);
+        
+}
+
+void removeTip(){}
 void replaceTip() { }
 void replaceNozzle() { }
 
@@ -10,8 +33,11 @@ void setup()
     Serial.begin(9600);
     while(!Serial)
     {
-        // wait for serial port to connect
+        // wait for serial coneection to establish
     }  
+
+    motor.attach(MOTOR_PIN);
+    pinMode(LIMIT_SWITCH, INPUT_PULLUP);
 }
 
 void loop() 
